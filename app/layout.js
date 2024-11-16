d762ee4d88a88a313fa817aab077618691e6a192
Ashish Madan <ashish@veersatech.com>
2024-11-16 16:19:28 +0530

    Followed naming convensions

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-import ClientThemeProvider from "@/components/ClientThemeProvider";
+import ClientThemeProvider from "@/components/client-theme-provider";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Header from "@/components/header";
