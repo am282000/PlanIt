7b39fa35f0253a5686a3c7b60467d26debe8e8a1
Ashish Madan <ashish@veersatech.com>
2024-11-17 22:14:58 +0530

    Checking of authenticated user

--- components/header.jsx
+++ components/header.jsx
@@ -4,8 +4,10 @@ import Link from "next/link";
 import { Button } from "./ui/button";
 import { PenBox } from "lucide-react";
 import UserMenu from "./user-menu";
+import { checkUser } from "@/lib/checkUser";
 
-const header = () => {
+const header = async () => {
+  await checkUser();
   return (
     <header className="container mx-auto">
       <nav className="py-2 px-4 flex justify-between items-center">
