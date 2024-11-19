c81e55d4bf1ca186c00a3984577180967177fffb
Ashish Madan <ashish@veersatech.com>
2024-11-20 05:10:36 +0530

    Added Toaster

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
 import { shadesOfPurple } from "@clerk/themes";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -37,6 +38,7 @@ export default function RootLayout({ children }) {
           <ClientThemeProvider>
             <Header />
             <main className="min-h-screen"> {children}</main>
+            <Toaster richColors/>
             <footer className="bg-gray-900 py-12">
               <div className="container mx-auto px-4 text-center text-gray-200">
                 <p>Made with ❤️ by Ashish Coder</p>
