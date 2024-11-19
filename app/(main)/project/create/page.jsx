4cbfb812c3fd8c5b433c3f38b3b4c20b7e362cef
Ashish Madan <ashish@veersatech.com>
2024-11-20 05:11:44 +0530

    Integrated Zod, React form hook , hookform resolvers for Creating a Project

--- app/(main)/project/create/page.jsx
+++ app/(main)/project/create/page.jsx
@@ -1,5 +1,126 @@
+"use client";
+import { createProject } from "@/actions/projects";
+import OrgSwitcher from "@/components/org-switcher";
+import { useOrganization, useUser } from "@clerk/nextjs";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { projectSchema } from "../../lib/validators";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import useFetch from "@/hooks/use-fetch";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+
 const CreateProjectPage = () => {
-  return <div>CreateProjectPage</div>;
+  const router = useRouter();
+  const { isLoaded: isOrgLoaded, membership } = useOrganization();
+  const { isLoaded: isUserLoaded } = useUser();
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(projectSchema),
+  });
+
+  const {
+    data: project,
+    loading,
+    error,
+    fn: createProjectFn,
+  } = useFetch(createProject);
+
+  const onSubmit = async (data) => {
+    console.log("Data: ", data);
+    createProjectFn(data);
+  };
+
+  useEffect(() => {
+    if (project) {
+      toast.success("Project created successfully!");
+      router.push(`/project/${project.id}`);
+    }
+  }, [loading]);
+
+  useEffect(() => {
+    if (isOrgLoaded && isUserLoaded && membership) {
+      setIsAdmin(membership.role === "org:admin");
+    }
+  }, [isOrgLoaded, isUserLoaded, membership]);
+
+  if (!isOrgLoaded || !isUserLoaded) {
+    return null;
+  }
+  // For members only - No acccess to create projects
+  if (!isAdmin) {
+    return (
+      <div className="flex flex-col gap-2 items-center">
+        <span className="text-2xl gradient-title">
+          Oops! Only admins can create projects.
+        </span>
+        <OrgSwitcher />
+      </div>
+    );
+  }
+  return (
+    <div className="container mx-auto py-10">
+      <h1 className="text-6xl text-center font-bold mb-8 gradient-title">
+        Create New Project
+      </h1>
+      <form
+        className="flex flex-col space-y-4"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <div>
+          <Input
+            id="name"
+            className="bg-slate-950"
+            placeholder="Project Name"
+            {...register("name")}
+          />
+          {errors.name && (
+            <p className="text-red-500 text-sm mt-1"> {errors.name.message}</p>
+          )}
+        </div>
+        <div>
+          <Input
+            id="key"
+            className="bg-slate-950"
+            placeholder="Project Key (Eg: AMTV)"
+            {...register("key")}
+          />
+          {errors.key && (
+            <p className="text-red-500 text-sm mt-1"> {errors.key.message}</p>
+          )}
+        </div>
+        <div>
+          <Input
+            id="description"
+            className="bg-slate-950 h-28"
+            placeholder="Project Description"
+            {...register("description")}
+          />
+          {errors.description && (
+            <p className="text-red-500 text-sm mt-1">
+              {errors.description.message}
+            </p>
+          )}
+        </div>
+        <Button
+          type="submit"
+          size="lg"
+          disabled={loading}
+          className="bg-blue-500 text-white hover:text-black"
+        >
+          {loading ? "Creating ..." : "Create Project"}
+        </Button>
+        {error && <p className="text-red-500 text-sm mt-1">{error.message}</p>}
+      </form>
+    </div>
+  );
 };
 
 export default CreateProjectPage;
