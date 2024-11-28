e6f971e3832ea8b41cc5db15b993d7743fcfa943
Ashish Madan <ashish@veersatech.com>
2024-11-28 19:56:22 +0530

    Added Kanban Board basic layout with Create issue Button

--- app/(main)/project/_components/sprint-board.jsx
+++ app/(main)/project/_components/sprint-board.jsx
@@ -1,11 +1,23 @@
 "use client";
 import { useState } from "react";
 import SprintManager from "./sprint-manager";
+import { DragDropContext, Droppable } from "@hello-pangea/dnd";
+import statuses from "@/data/status.json";
+import { Plus } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 const SprintBoard = ({ sprints, projectId, orgId }) => {
   const defaultSprint =
     sprints.find((s) => s.status === "ACTIVE") || sprints[0];
   const [currentSprint, setCurrentSprint] = useState(defaultSprint);
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const [selectedStatus, setSelectedStatus] = useState(null);
+
+  const onDragEnd = () => {};
+  const handleAddIssue = (status) => {
+    setSelectedStatus(status);
+    setIsDrawerOpen(true);
+  };
   return (
     <div>
       {/* SPRINT MANAGER */}
@@ -16,6 +28,42 @@ const SprintBoard = ({ sprints, projectId, orgId }) => {
         projectId={projectId}
       />
       {/* KANBAN BOARD */}
+      <DragDropContext onDragEnd={onDragEnd}>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mt-4 bg-slate-900 p-4 rounded-1g">
+          {statuses.map((column) => (
+            <Droppable key={column.key} droppableId={column.key}>
+              {(provided) => {
+                return (
+                  <div
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                    className="space-y-2"
+                  >
+                    <h3 className="font-semibold mb-2 text-center">
+                      {column.name}
+                    </h3>
+                    {/* Issues */}
+                    {provided.placeholder}
+                    {column.key === "TODO" &&
+                      currentSprint.status !== "COMPLETED" && (
+                        <Button
+                          variant="ghost"
+                          className="w-full"
+                          onClick={() => handleAddIssue(column.key)}
+                        >
+                          <Plus className="mr-2 h-4 w-4" />
+                          Create Issue
+                        </Button>
+                      )}
+                  </div>
+                );
+              }}
+            </Droppable>
+          ))}
+        </div>
+      </DragDropContext>
+
+      {/* Create Issue Drawer */}
     </div>
   );
 };
