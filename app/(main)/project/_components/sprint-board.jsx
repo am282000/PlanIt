f3f2730f7a0a47eb8abeb265eabeb4892cc52d04
Ashish Madan <ashish@veersatech.com>
2024-12-03 16:07:39 +0530

    Added Issues in Kanban

--- app/(main)/project/_components/sprint-board.jsx
+++ app/(main)/project/_components/sprint-board.jsx
@@ -1,10 +1,15 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SprintManager from "./sprint-manager";
-import { DragDropContext, Droppable } from "@hello-pangea/dnd";
+import { DragDropContext, Draggable, Droppable } from "@hello-pangea/dnd";
 import statuses from "@/data/status.json";
 import { Plus } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import CreateIssueDrawer from "./create-issue";
+import useFetch from "@/hooks/use-fetch";
+import { getIssuesForSprint } from "@/actions/issues";
+import { BeatLoader } from "react-spinners";
+import IssueCard from "@/components/isssue-card";
 
 const SprintBoard = ({ sprints, projectId, orgId }) => {
   const defaultSprint =
@@ -14,10 +19,32 @@ const SprintBoard = ({ sprints, projectId, orgId }) => {
   const [selectedStatus, setSelectedStatus] = useState(null);
 
   const onDragEnd = () => {};
+
+  const {
+    data: issues,
+    loading: issuesLoading,
+    error: issuesError,
+    fn: fetchIssues,
+    setData: setIssues,
+  } = useFetch(getIssuesForSprint);
+  const [filterdIssues, setFilterdIssues] = useState(issues);
+
+  useEffect(() => {
+    if (currentSprint.id) {
+      fetchIssues(currentSprint.id);
+    }
+  }, [currentSprint.id]);
+
+  const handleIssueCreated = () => {
+    // Fetch issues again
+    fetchIssues(currentSprint.id);
+  };
   const handleAddIssue = (status) => {
     setSelectedStatus(status);
     setIsDrawerOpen(true);
   };
+
+  if (issuesError) return <div>Error loading Issues</div>;
   return (
     <div>
       {/* SPRINT MANAGER */}
@@ -27,6 +54,9 @@ const SprintBoard = ({ sprints, projectId, orgId }) => {
         sprints={sprints}
         projectId={projectId}
       />
+      {issuesLoading && (
+        <BeatLoader className="mt-4" width={"100%"} color="#36d7b7" />
+      )}
       {/* KANBAN BOARD */}
       <DragDropContext onDragEnd={onDragEnd}>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mt-4 bg-slate-900 p-4 rounded-1g">
@@ -43,6 +73,27 @@ const SprintBoard = ({ sprints, projectId, orgId }) => {
                       {column.name}
                     </h3>
                     {/* Issues */}
+                    {issues
+                      ?.filter((issue) => issue.status === column.key)
+                      .map((issue, index) => (
+                        <Draggable
+                          key={issue.id}
+                          draggableId={issue.id}
+                          index={index}
+                        >
+                          {(provided) => {
+                            return (
+                              <div
+                                ref={provided.innerRef}
+                                {...provided.droppableProps}
+                                {...provided.dragHandleProps}
+                              >
+                                <IssueCard issue={issue} />
+                              </div>
+                            );
+                          }}
+                        </Draggable>
+                      ))}
                     {provided.placeholder}
                     {column.key === "TODO" &&
                       currentSprint.status !== "COMPLETED" && (
@@ -64,6 +115,15 @@ const SprintBoard = ({ sprints, projectId, orgId }) => {
       </DragDropContext>
 
       {/* Create Issue Drawer */}
+      <CreateIssueDrawer
+        isOpen={isDrawerOpen}
+        onClose={() => setIsDrawerOpen(false)}
+        sprintId={currentSprint.id}
+        status={selectedStatus}
+        projectId={projectId}
+        onIssueCreated={handleIssueCreated}
+        orgId={orgId}
+      />
     </div>
   );
 };
