ec1f36d3d573c690de815bacc1af3b5511a5ae6f
Ashish Madan <ashish@veersatech.com>
2024-12-04 05:55:54 +0530

    UpdateIssueOrder, deleteIssue Actions added

--- actions/issues.js
+++ actions/issues.js
@@ -71,3 +71,79 @@ export async function getIssuesForSprint(sprintId) {
 
   return issues;
 }
+
+/**
+ * Check for authorized users
+ * Create a transaction with prisma instance and loop updatedIssues
+ * Update status, order for issue
+ */
+
+export async function UpdateIssueOrder(updatedIssues) {
+  const { userId, orgId } = auth();
+  if (!userId || !orgId) {
+    throw new Error("Unauthorized user");
+  }
+  await db.$transaction(async (prisma) => {
+    for (const issue of updatedIssues) {
+      await prisma.issue.update({
+        where: {
+          id: issue.id,
+        },
+        data: {
+          status: issue.status,
+          order: issue.order,
+        },
+      });
+    }
+  });
+
+  return { success: true };
+}
+
+/**
+ * Delete issue - check for authorized users
+ * Find user details
+ * Find issue with project true
+ * Only reporter and admins can delete it
+ * Delete from DB
+ */
+
+export async function deleteIssue(issueId) {
+  const { userId, orgId } = auth();
+  if (!userId || !orgId) {
+    throw new Error("Unauthorized user");
+  }
+  const user = await db.user.findUnique({
+    where: {
+      clerkUserId: userId,
+    },
+  });
+  if (!user) {
+    throw new Error("User not Found");
+  }
+  const issue = await db.issue.findUnique({
+    where: {
+      id: issueId,
+    },
+    include: {
+      project: true,
+    },
+  });
+  if (!issue) {
+    throw new Error("Issue not found");
+  }
+  if (
+    issue.reporterId !== user.id &&
+    !issue.project.adminIds.includes(user.id)
+  ) {
+    throw new Error("You don't have permission to delete this issue");
+  }
+
+  await db.issue.delete({
+    where: {
+      id: issueId,
+    },
+  });
+
+  return { success: true };
+}
