a25a228d7610864bbaec4c1b767c1bc37f0af88c
Ashish Madan <ashish@veersatech.com>
2024-12-07 00:19:34 +0530

    Show filters on issues length

--- app/(main)/project/_components/board-filters.jsx
+++ app/(main)/project/_components/board-filters.jsx
@@ -50,63 +50,66 @@ const BoardFilters = ({ issues, onFilterChange }) => {
     });
     onFilterChange(filteredIssues);
   }, [searchTerm, selectedAssignees, selectedPriority, issues]);
+  console.log("issues: ", issues);
 
   return (
     <div>
-      <div className="flex flex-col gap-4 pr-2 sm:flex-row sm:gap-6 mt-4 ">
-        <Input
-          className="w-full sm:w-72"
-          placeholder="Search issues"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-        <div className="flex-shrink-0">
-          <div className="flex gap-2 flex-wrap">
-            {uniqueAssignees.map((assignee, index) => {
-              const selected = selectedAssignees.includes(assignee.id);
-              return (
-                <div
-                  key={assignee.id}
-                  className={`rounded-full ring ${
-                    selected ? "ring-blue-600" : "ring-black"
-                  } ${index > 0 ? "-ml-6" : ""}`}
-                  style={{
-                    zIndex: index,
-                  }}
-                  onClick={() => toggleAssignee(assignee.id)}
-                >
-                  <Avatar>
-                    <AvatarImage src={assignee.imageUrl} />
-                    <AvatarFallback>{assignee.name}</AvatarFallback>
-                  </Avatar>
-                </div>
-              );
-            })}
+      {issues?.length !== 0 && (
+        <div className="flex flex-col gap-4 pr-2 sm:flex-row sm:gap-6 mt-4 ">
+          <Input
+            className="w-full sm:w-72"
+            placeholder="Search issues"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <div className="flex-shrink-0">
+            <div className="flex gap-2 flex-wrap">
+              {uniqueAssignees.map((assignee, index) => {
+                const selected = selectedAssignees.includes(assignee.id);
+                return (
+                  <div
+                    key={assignee.id}
+                    className={`rounded-full ring ${
+                      selected ? "ring-blue-600" : "ring-black"
+                    } ${index > 0 ? "-ml-6" : ""}`}
+                    style={{
+                      zIndex: index,
+                    }}
+                    onClick={() => toggleAssignee(assignee.id)}
+                  >
+                    <Avatar>
+                      <AvatarImage src={assignee.imageUrl} />
+                      <AvatarFallback>{assignee.name}</AvatarFallback>
+                    </Avatar>
+                  </div>
+                );
+              })}
+            </div>
           </div>
-        </div>
 
-        <Select value={selectedPriority} onValueChange={setSelectedPriority}>
-          <SelectTrigger className="w-full sm:w-52">
-            <SelectValue placeholder="Select Priority" />
-          </SelectTrigger>
-          <SelectContent>
-            {priorities.map((priority) => (
-              <SelectItem key={priority} value={priority}>
-                {priority}
-              </SelectItem>
-            ))}
-          </SelectContent>
-        </Select>
-        {isFilterApplied && (
-          <Button
-            variant="ghost"
-            onClick={clearFilters}
-            className="flex items-center bg-slate-700"
-          >
-            <X className=" h-4 w-4 mr-1" /> Clear Fillters
-          </Button>
-        )}
-      </div>
+          <Select value={selectedPriority} onValueChange={setSelectedPriority}>
+            <SelectTrigger className="w-full sm:w-52">
+              <SelectValue placeholder="Select Priority" />
+            </SelectTrigger>
+            <SelectContent>
+              {priorities.map((priority) => (
+                <SelectItem key={priority} value={priority}>
+                  {priority}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+          {isFilterApplied && (
+            <Button
+              variant="ghost"
+              onClick={clearFilters}
+              className="flex items-center bg-slate-700"
+            >
+              <X className=" h-4 w-4 mr-1" /> Clear Fillters
+            </Button>
+          )}
+        </div>
+      )}
     </div>
   );
 };
