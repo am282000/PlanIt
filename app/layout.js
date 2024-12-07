f7b59b7e226886e46509cc5b071c1bad337f03c1
Ashish Madan <ashish@veersatech.com>
2024-12-08 04:37:36 +0530

    Fix board overflow issue

--- app/layout.js
+++ app/layout.js
@@ -40,7 +40,7 @@ export default function RootLayout({ children }) {
         <body className={`${inter.className} dotted-background`}>
           <ClientThemeProvider>
             <Header />
-            <main className="min-h-screen"> {children}</main>
+            <main className="min-h-screen mb-7"> {children}</main>
             <Toaster richColors />
             <footer className="bg-gray-900 py-12">
               <div className="container mx-auto px-4 text-center text-gray-200">
